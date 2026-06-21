b94539dc09cb54b3b6e7d984a0b30387d4553db3
Gayatri <work.gayatrihedau@gmail.com>
2026-06-21 16:52:09 +0530

    vendor issue in Grocery_purchase_model

--- application/webservice/grocery_purchase/models/Grocery_purchase_model.php
+++ application/webservice/grocery_purchase/models/Grocery_purchase_model.php
@@ -1,18 +1,21 @@
 <?php
-class Grocery_purchase_model extends CI_Model {
+class Grocery_purchase_model extends CI_Model
+{
     private $table = 'grocery_purchases';
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->database();
     }
 
-    public function get_all($limit = 10, $offset = 0, $filters = []) {
-        $this->db->select('p.*, s.shop_name, i.item_name, i.unit, v.vendor_name, u.name as added_by_name');
+    public function get_all($limit = 10, $offset = 0, $filters = [])
+    {
+        $this->db->select('p.*, s.shop_name, i.item_name, i.unit, u.name as added_by_name');
         $this->db->from($this->table . ' p');
         $this->db->join('shops s', 's.id = p.shop_id', 'left');
         $this->db->join('grocery_items i', 'i.id = p.grocery_item_id', 'left');
-        $this->db->join('vendors v', 'v.id = p.vendor_id', 'left');
+        // $this->db->join('vendors v', 'v.id = p.vendor_id', 'left');
         $this->db->join('users u', 'u.id = p.added_by', 'left');
         $this->db->where('p.is_delete', '0');
 
@@ -33,7 +36,8 @@ class Grocery_purchase_model extends CI_Model {
         return $this->db->get()->result_array();
     }
 
-    public function get_count($filters = []) {
+    public function get_count($filters = [])
+    {
         $this->db->from($this->table . ' p');
         $this->db->where('p.is_delete', '0');
 
@@ -51,31 +55,35 @@ class Grocery_purchase_model extends CI_Model {
         return $this->db->count_all_results();
     }
 
-    public function get_by_id($id) {
-        $this->db->select('p.*, s.shop_name, i.item_name, i.unit, v.vendor_name, u.name as added_by_name');
+    public function get_by_id($id)
+    {
+        $this->db->select('p.*, s.shop_name, i.item_name, i.unit, u.name as added_by_name');
         $this->db->from($this->table . ' p');
         $this->db->join('shops s', 's.id = p.shop_id', 'left');
         $this->db->join('grocery_items i', 'i.id = p.grocery_item_id', 'left');
-        $this->db->join('vendors v', 'v.id = p.vendor_id', 'left');
+        // $this->db->join('vendors v', 'v.id = p.vendor_id', 'left');
         $this->db->join('users u', 'u.id = p.added_by', 'left');
         $this->db->where('p.id', $id);
         $this->db->where('p.is_delete', '0');
         return $this->db->get()->row_array();
     }
 
-    public function insert($data) {
+    public function insert($data)
+    {
         $data['added_date'] = date('Y-m-d H:i:s');
         $this->db->insert($this->table, $data);
         return $this->db->insert_id();
     }
 
-    public function update($id, $data) {
+    public function update($id, $data)
+    {
         $data['updated_date'] = date('Y-m-d H:i:s');
         $this->db->where('id', $id);
         return $this->db->update($this->table, $data);
     }
 
-    public function delete($id, $updated_by) {
+    public function delete($id, $updated_by)
+    {
         $data = [
             'is_delete' => '1',
             'updated_by' => $updated_by,
